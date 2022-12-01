3d7f715601a665879195fdf5d0905da06c37d25e
louis-marceron <72874947+louis-marceron@users.noreply.github.com>
2022-12-01 12:34:55 +0100

    commit

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,17 @@
 import dotenv from 'dotenv'
-import express, {NextFunction, Request, Response} from 'express'
-import path from 'path'
+import express, {Express, Request, Response} from 'express'
 
+// Load environment variables from .env (variables globally available in process.env)
 dotenv.config({path: '.env'})
 
 // Create the router
-const app = express()
+const app: Express = express()
 // Get the listening port of the router in the .env file (3000 by default)
 const port = process.env.PORT || 3000
 
 // Allow to parse JSON in the body of the request
 app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({extended: false}))
 
 // Set view engine to ejs
 app.set('view engine', 'ejs')
@@ -26,22 +26,22 @@ app.get('/', (req, res) => {
 */
 
 app.get('/', (req: Request, res: Response) => {
-	res.render('index.ejs')
+    res.render('index.ejs')
 })
 
 app.get('/news', (req: Request, res: Response) => {
-	res.render('news.ejs')
+    res.render('news.ejs')
 })
 
 app.get('/contact', (req: Request, res: Response) => {
-	res.render('contact.ejs')
+    res.render('contact.ejs')
 })
 
 app.get('/about', (req: Request, res: Response) => {
-	res.render('about.ejs')
+    res.render('about.ejs')
 })
 
-// When the server is loaded and and starts listening
+// When the server is loaded and starts listening
 app.listen(port, () => {
-	return console.log(`Express is listening at http://localhost:${port}`);
+    return console.log(`[Server]: Express is listening at http://localhost:${port}`);
 })
